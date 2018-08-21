03bb317473d3000bbf2a08e0a743119259043959
GlauberC <glaubercarv@gmail.com>
2018-08-21 13:42:03 -0300

    lista 2 de IOT

--- internet_das_coisas/lista2/js/lista1.js
+++ internet_das_coisas/lista2/js/lista1.js
@@ -1,3 +1,14 @@
 function mudar(){
-	alert('Execultou o exercicio');
+	msg = "0 - mudou<br/>";
+	for (var i = 1; i<11; i++ ){
+		msg = msg + i + ' - ' + 'mudou<br/>';
+	}
+	document.getElementById("result").innerHTML = msg;
+}
+
+function liga_lamp(){
+	document.getElementById("luz1").src = "imagens/Lon.png"
+}
+function desliga_lamp(){
+	document.getElementById("luz1").src = "imagens/Loff.png"
 }
